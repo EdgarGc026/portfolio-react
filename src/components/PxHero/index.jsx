1c4cc97281560db1b4cca061d82961087bc20688
Edgar Gonzalez <muertsubita40@hotmail.com>
2023-02-24 12:06:53 -0600

    [WEBPORTFOLIO_009-refactor-style-component-I] feat: heroComponent refactor v1

--- src/components/PxHero/index.jsx
+++ src/components/PxHero/index.jsx
@@ -22,24 +22,36 @@ const PxHero = () => {
     return <></>;
   }
 
+  const variantMap = {
+    header: "h-[500px]",
+    layout: "sm:grid-layout grid-layout-mobile",
+    firstColumn: "col-span-6 pr-5 text-justify font-raleway",
+    title: "font-semibold text-[2.5rem]",
+    description: "font-normal",
+    hidden: "parraph",
+    button: "btn-cv",
+    secondColumn: "w-full h-auto col-span-6",
+    image: "sm:my-[20px] w-[95%] max-w-[450px] sm:mx-0 grid"
+  };
+
   return (
     <>
-      <header className="h-[500px]">
-        <div className="sm:grid-layout grid-layout-mobile">
-          <div className="col-span-6 pr-5 text-justify font-raleway">
-            <h1 className="font-semibold text-[2.5rem]">
+      <header className={variantMap.header}>
+        <div className={variantMap.layout}>
+          <div className={variantMap.firstColumn}>
+            <h1 className={variantMap.title}>
               {headerData.name} <br />
-              <span className="font-normal">{headerData.profession}</span>
+              <span className={variantMap.description}>{headerData.profession}</span>
             </h1>
-            <p className="parraph"></p>
+            <p className={variantMap.hidden}></p>
             <a
               href="https://drive.google.com/file/d/1mFTbUjSFcJ-gwbdTixT7M6jMHruNXyFY/view?usp=sharing"
-              className="btn-cv"
+              className={variantMap.button}
               target="_blank"
             >Get CV</a>
           </div>
-          <div className="w-full h-auto col-span-6">
-            <img className="sm:my-[20px] w-[95%] max-w-[450px] sm:mx-0 grid" src={heroLogo} alt="hero-image" />
+          <div className={variantMap.secondColumn}>
+            <img className={variantMap.image} src={heroLogo} alt="hero-image" />
           </div>
         </div>
       </header>
