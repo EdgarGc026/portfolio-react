10faaacbc381bd09b7296d117e9a2440b8b751c0
Edgar Gonzalez <muertsubita40@hotmail.com>
2023-02-24 15:30:20 -0600

    WEBPORTFOLIO_009-refactor-style-component-I SkillCard  refactor style

--- src/components/PxSkill/index.jsx
+++ src/components/PxSkill/index.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import SxSkillCard from "./SxSkillCard";
 
 const PxSkill = () => {
+  const variantMap = {
+    title: "mb-5 text-[2.5rem] text-center font-semibold",
+    layout: "grid-layout grid-layout-mobile"
+  };
+
   return (
     <>
       <section>
-        <h2 className="mb-5 text-[2.5rem] text-center font-semibold">Skills</h2>
-        <section className="grid-layout grid-layout-mobile">
+        <h2 className={variantMap.title}>Skills</h2>
+        <section className={variantMap.layout}>
           <SxSkillCard />
         </section>
       </section>
