185a145f417a4ccc08832ef56e1203ac7c07e7ce
Edgar Gonzalez <muertsubita40@hotmail.com>
2023-02-24 15:28:14 -0600

    WEBPORTFOLIO_009-refactor-style-component-I SkillCard and skillCardComponent refactor style

--- src/components/PxSkill/SxSkillCard/index.jsx
+++ src/components/PxSkill/SxSkillCard/index.jsx
@@ -26,19 +26,28 @@ const SxSkillCard = () => {
     return <></>;
   }
 
+  const variantMap = {
+    container: "w-full h-auto col-span-full flex justify-evenly gap-5 items-center flex-wrap my-4",
+    card: "sm:w-[30%] w-[90%] h-[220px] py-5 px-3 sm:flex-shrink-0 bg-just-white shadow-card-shadow text-center",
+    image: "w-[50px] h-[50px] inline-block text-[50px] text-center overflow-visible box-content",
+    icon: "text-[50px] text-center",
+    title: "font-semibold sm:text-lg text-xl sm:my-5 m-[15px]",
+    description: "hidden"
+  };
+
   return (
     <>
-      <div className="w-full h-auto col-span-full flex justify-evenly gap-5 items-center flex-wrap my-4">
+      <div className={variantMap.container}>
         {skillCardData.map((item, index) => {
           return (
-            <div className="sm:w-[30%] w-[90%] h-[220px] py-5 px-3 sm:flex-shrink-0 bg-just-white shadow-card-shadow text-center" key={item.id}>
+            <div className={variantMap.card} key={item.id}>
               {
                 iconsName[index] == missingIcon
-                  ? <img className="w-[50px] h-[50px] inline-block text-[50px] text-center overflow-visible box-content" src={GrailsSVG} alt="grails-icon" />
-                  : <FontAwesomeIcon icon={["fab", iconsName[index]]} className="text-[50px] text-center" />
+                  ? <img className={variantMap.image} src={GrailsSVG} alt="grails-icon" />
+                  : <FontAwesomeIcon icon={["fab", iconsName[index]]} className={variantMap.icon} />
               }
-              <h3 className="font-semibold sm:text-lg text-xl sm:my-5 m-[15px]">{item.name}.</h3>
-              <p className="hidden">{item.description}.</p>
+              <h3 className={variantMap.title}>{item.name}.</h3>
+              <p className={variantMap.description}>{item.description}.</p>
             </div>
           );
         })
