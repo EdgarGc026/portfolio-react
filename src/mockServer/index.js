0cd8c1cd39199c52b6d36a706c9fff5c71523840
Edgar Gonzalez <muertsubita40@hotmail.com>
2023-03-04 20:42:36 -0600

    [WEBPORTFOLIO_017-storybook-footer-component] feat: footer migrate v2

--- src/mockServer/index.js
+++ src/mockServer/index.js
@@ -1,5 +1,14 @@
 import { createServer } from "miragejs";
 
+const socialMediaButtonGroup = [
+  { "id": 1, "name": "github", "url": "https://github.com/edgargc026" },
+  {
+    "id": 2, "name": "linkedin-in", "url": "https://www.linkedin.com/in/edgargc026/"
+  },
+  { "id": 3, "name": "twitter", "url": "https://twitter.com/edgargc026" },
+  { "id": 4, "name": "instagram", "url": "https://instagram.com/edgargc026" }
+];
+
 export const mockServer = createServer({
   routes() {
     this.get("/buttons/:variant", (schema, request) => {
@@ -7,6 +16,13 @@ export const mockServer = createServer({
         data: {
           text: `${request.params.variant}`,
           variant: request.params.variant
+        }
+      };
+    });
+    this.get(`/socialMediaButtonGroup`, (schema, request) => {
+      return {
+        data: {
+          socialMediaButtonGroup
         }
       };
     });
