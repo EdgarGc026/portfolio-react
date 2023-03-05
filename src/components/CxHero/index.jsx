8b023b59ebfc54a3dc9373fd095fb851c248383f
Edgar Gonzalez <muertsubita40@hotmail.com>
2023-03-04 18:16:57 -0600

    [WEBPORTFOLIO_013] feat: Hero component storybook added

---
+++ src/components/CxHero/index.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import PropTypes from "prop-types";
+// import { Link } from "react-router-dom";
+import Button from "../CxButton";
+import heroLogo from "../../assets/hero.svg";
+
+const Hero = ({ name, profession, buttonText, buttonProps, ...props }) => {
+  const { url } = buttonProps;
+  const variantStyle = {
+    header: "h-[500px]",
+    layout: "sm:grid-layout grid-layout-mobile",
+    firstColumn: "col-span-6 pr-5 text-justify font-raleway",
+    title: "font-semibold text-[2.5rem]",
+    description: "font-normal",
+    hidden: "parraph",
+    button: "button-hero",
+    secondColumn: "w-full h-auto col-span-6",
+    image: "sm:my-[20px] w-[95%] max-w-[450px] sm:mx-0 grid"
+  };
+
+  return (
+    <div>
+      <header className={variantStyle.header} {...props}>
+        <div className={variantStyle.layout}>
+          <div className={variantStyle.firstColumn}>
+            <h1 className={variantStyle.title}>
+              {name} <br />
+              <span className={variantStyle.description}>{profession}</span>
+            </h1>
+            <p className={variantStyle.hidden}></p>
+            <a href={url} className={variantStyle.button}>{buttonText}</a>
+          </div>
+          <div className={variantStyle.secondColumn}>
+            <img className={variantStyle.image} src={heroLogo} alt="hero-image" />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+Hero.propTypes = {
+  name: PropTypes.string,
+  profession: PropTypes.string,
+  buttonText: PropTypes.string,
+  buttonProps: PropTypes.object,
+};
+
+Hero.defaultProps = {
+  name: "",
+  profession: "",
+  buttonText: "",
+  buttonProps: undefined
+};
+
+export default Hero;
--- src/mockServer/index.js
+++ src/mockServer/index.js
@@ -1,5 +1,15 @@
 import { createServer } from "miragejs";
 
+const heroComponent = {
+  name: "Edgar Gonzalez",
+  profession: "Backend developer",
+  button: {
+    variant: "heroButton",
+    buttonText: "Get Cv",
+    url: "https://drive.google.com/file/d/1mFTbUjSFcJ-gwbdTixT7M6jMHruNXyFY/view?usp=sharing"
+  }
+};
+
 export const mockServer = createServer({
   routes() {
     this.get("/buttons/:variant", (schema, request) => {
@@ -9,6 +19,10 @@ export const mockServer = createServer({
           variant: request.params.variant
         }
       };
+    });
+
+    this.get("/heroComponent", (schema, request) => {
+      return heroComponent;
     });
   }
 });
