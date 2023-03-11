75f981c0be21f2c9e6a36f784a2f4a42d922677d
Edgar Gonzalez <muertsubita40@hotmail.com>
2023-03-10 19:32:15 -0600

    [WEBPORTFOLIO_017-storybook-page-home-component] feat v3 complete

---
+++ src/components/SxPageHome/index.jsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { Hero, Card, About, Portfolio, SocialMediaButtonGroup } from ".."
+
+const PageHome = ({ pageHomeData }) => {
+  return (
+    <div>
+      <Hero
+        name={pageHomeData.heroComponent.name}
+        profession={pageHomeData.heroComponent.profession}
+        buttonText={pageHomeData.heroComponent.button.buttonText}
+        buttonProps={pageHomeData.heroComponent.button}
+      />
+      <div>
+        <h2 className="mb-5 text-[2.5rem] text-center font-semibold">Skill</h2>
+        <section className="grid-layout grid-layout-mobile">
+          <Card skillArray={pageHomeData.skills} />
+        </section>
+      </div>
+      <About description={pageHomeData.aboutMe[0].description} />
+      <section className="p-[15px] sm:p-[60px] grid-layout">
+        <div className="col-span-full row-span-full mb-5">
+          <h2 className="mb-5 text-[2.5rem] text-center font-semibold">Portfolio</h2>
+          <Portfolio portfolioArray={pageHomeData.portfolioProjects} />
+        </div>
+      </section>
+      <SocialMediaButtonGroup socialMediaIcons={pageHomeData.socialMediaButtonGroup} />
+    </div>
+  )
+}
+
+export default PageHome
